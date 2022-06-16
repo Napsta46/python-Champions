5c951f97d6e80997e13fb78504d311e04373b135
Andy <abrahamandikan46@outlook.com>
2022-06-16 14:44:13 +0100

    adding color with hex code #A582F9 on welcome bar

--- github code.py
+++ github code.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-
+BG_COlOR = "#A582F9"
 class PythonChampions:
     def __init__(self):
         self.window = Tk()
@@ -14,7 +14,7 @@ class PythonChampions:
         self.window.resizable(width=False, height=False)
         self.window.configure(width=470, height=100)
         
-        head_label = Label(self.window,text="Welcome",  pady = 10)
+        head_label = Label(self.window,text="Welcome", pady = 10, bg = BG_COlOR)
         head_label.place(relwidth=1)
 
 
